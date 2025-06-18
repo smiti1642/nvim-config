3aad80e162de6fe25d19351f37dd4cf60f223df3
smiti1642 <56190474+smiti1642@users.noreply.github.com>
2025-06-18 11:26:17 +0800

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,7 +21,22 @@ return {
       vim.g.copilot_assume_mapped = true; -- Assume that the mapping is already done
     end
   },
-
+  
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    
+    -- remove if you use neither macOS or Linux
+    -- build = "make tiktoken", -- Only on macOS or Linux
+    
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
